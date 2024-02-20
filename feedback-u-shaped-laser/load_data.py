36d92fa0870d7835e43181767850ccae6d434da0
Simon Thorndahl Thomsen <sthomsen@ece.au.dkm>
2024-02-20 11:55:16 +0100

    Restructured all functions from main script to external python files

---
+++ feedback-u-shaped-laser/load_data.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Functions for loading and plotting raw data
+
+def get_esa_data(path):
+    #Returns:
+    #freqs: frequency axis in MHz
+    #powers: ESA power in dBm 
+    data = np.loadtxt(path)
+    freqs = data[0,:]*1e-6
+    powers = data[1,:]
+    return freqs, powers
+
+def get_data_from_folder(directory):
+    files = os.listdir(directory)
+    def path(file):
+        return directory + '\\' + file
+    return ([path(files[1]), path(files[3])], [path(files[5]), path(files[7])], [path(files[8])])
+
+def plot_spectrum(freqs,powers,label=''):
+    plt.plot(freqs,powers,label=label)
+    plt.xlabel('Fourier frequency [MHz]')
+    plt.ylabel('ESA power [dBm]')
