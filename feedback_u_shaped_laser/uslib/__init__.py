4065715fa1f2839aea2e14f76aba2ea1c2e5c376
Jeppe Heering Surrow <au617810@uni.au.dk>
2024-02-27 10:40:16 +0100

    Made uslib into a package

---
+++ feedback_u_shaped_laser/uslib/__init__.py
@@ -0,0 +1,7 @@
+'''from uslib import load_data
+import fit_functions
+import data_processing
+
+ld = load_data.LoadData()
+ff = fit_functions.fit_functions()
+dp = data_processing.data_processing()'''
