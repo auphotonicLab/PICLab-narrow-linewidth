79090f23ae20c6385c4fdc92c7c40a6fe7167a1e
Simon Thorndahl Thomsen <sthomsen@ece.au.dkm>
2024-03-21 11:02:02 +0100

    Revert "Delete feedback_u_shaped_laser/uslib directory"
    This reverts commit 2b90cae1d0c3f499c51fde3083947a6f272fb87c.

---
+++ feedback_u_shaped_laser/uslib/__init__.py
@@ -0,0 +1,8 @@
+from uslib import load_data, fit_functions, data_processing
+
+"""Package for u-shaped laser data analysis
+"""
+
+ld = load_data.LoadData()
+ff = fit_functions.FitFunctions()
+dp = data_processing.DataProcessing()
