2b90cae1d0c3f499c51fde3083947a6f272fb87c
SimonThorndahlThomsen <142304855+SimonThorndahlThomsen@users.noreply.github.com>
2024-03-21 10:52:46 +0100

    Delete feedback_u_shaped_laser/uslib directory
    The directory should be found in U_shaped_laser_package instead

--- feedback_u_shaped_laser/uslib/__init__.py
+++
@@ -1,8 +0,0 @@
-from uslib import load_data, fit_functions, data_processing
-
-"""Package for u-shaped laser data analysis
-"""
-
-ld = load_data.LoadData()
-ff = fit_functions.FitFunctions()
-dp = data_processing.DataProcessing()
