cd86458a79301649477ab11862e108a04e995f48
jsurrow <jeppe@surrow.com>
2026-09-10 16:09:19 +0200

    Updated solutions

--- QTLab/EMode_coupling_ring_resonator.py
+++ QTLab/EMode_coupling_ring_resonator.py
@@ -18,11 +18,17 @@ w_core = 1140        # [nm] waveguide core width
 h_core = 350         # [nm] waveguide core height (fixed for this platform)
 num_modes = 2        # [-] number of modes
 
+
+gap = 1040
+
+d = (w_core + gap) / 2   # centre-to-centre offset of each core from x=0
+
+
 # cladding thickness. Just needs to exceed the mode's decay length. 
 # Top cladding is thin (only 100 nm of oxide before air); 
 # side and bottom just need to be big enough for the mode to have decayed to ~0 by the window edge.
 
-w_clad = 1500        # [nm] side cladding thickness (SiO2), each side
+w_clad = 1500 + d    # [nm] side cladding thickness (SiO2), each side
 h_clad = 1500        # [nm] bottom cladding thickness (SiO2)
 h_clad_top = 100     # [nm] top cladding thickness (SiO2)
 h_air = 1500         # [nm] air above the top cladding, up to the window edge
@@ -30,7 +36,7 @@ h_air = 1500         # [nm] air above the top cladding, up to the window edge
 r_ring = 75e-6       # [µm] racetrack bend radius
 lc_racetrack = 20e-6 # [µm] racetrack straight (coupling) section length
 
-gap = 1040
+
 
 
 # %%
@@ -39,55 +45,29 @@ window_width = w_core + 2 * w_clad
 clad_height = h_clad + h_core + h_clad_top   # bottom clad + core + thin top clad
 window_height = clad_height + h_air
 
-
 em = emc.EMode(simulation_name='modes', clear='mine')
 em.settings(wavelength=wavelength, x_resolution=dx, y_resolution=dy,
             window_width=window_width, window_height=window_height,
             num_modes=num_modes, background_material='Air')
 
 em.shape(name='clad', material='SiO2', width=window_width, height=clad_height, position=[0, clad_height / 2])
-em.shape(name='core', material='SiN', width=w_core, height=h_core, position=[0, h_clad + h_core / 2])
+em.shape(name='core1', material='SiN', width=w_core, height=h_core, position=[-d, h_clad + h_core / 2])
+
+em.shape(name='core2', material='SiN', width=w_core, height=h_core, position=[d, h_clad + h_core / 2])
 
 em.FDM()
 report = em.report()
 
 em.plot()
 
+# em.plot(component='Ex', mode=0, file_name='mode_symmetric', file_type='png')
+# em.plot(component='Ex', mode=1, file_name='mode_antisymmetric', file_type='png')
+# em.close()
 
-core_y_bottom = BOTTOM_MARGIN_NM + BOX_NM
-core_y_centre = core_y_bottom + HEIGHT_NM / 2
-d = (WIDTH_NM + GAP_NM) / 2   # centre-to-centre offset of each core from x=0
-
-half_span = d + WIDTH_NM / 2
-window_width = 2 * (half_span + CLAD_NM + SIDE_MARGIN_NM)
-window_height = BOTTOM_MARGIN_NM + BOX_NM + HEIGHT_NM + CLAD_NM + TOP_MARGIN_NM
-clad_width = 2 * d + WIDTH_NM + 2 * CLAD_NM
-
-em = emc.EMode(simulation_name='coupler', clear='mine')
-em.settings(wavelength=WAVELENGTH_NM, x_resolution=10, y_resolution=10,
-            window_width=window_width, window_height=window_height,
-            num_modes=2, background_material='Air')
-
-em.shape(name='box', material='SiO2', width=window_width, height=BOTTOM_MARGIN_NM + BOX_NM,
-          position=[0, (BOTTOM_MARGIN_NM + BOX_NM) / 2])
-em.shape(name='clad', material='SiO2', width=clad_width, height=HEIGHT_NM + CLAD_NM,
-          position=[0, core_y_bottom + (HEIGHT_NM + CLAD_NM) / 2])
-em.shape(name='core1', material='SiN', width=WIDTH_NM, height=HEIGHT_NM, position=[-d, core_y_centre])
-em.shape(name='core2', material='SiN', width=WIDTH_NM, height=HEIGHT_NM, position=[d, core_y_centre])
-
-em.plot(component='n', file_name='geometry', file_type='png')
-
-em.FDM()
-report = em.report()
-
-em.plot(component='Ex', mode=0, file_name='mode_symmetric', file_type='png')
-em.plot(component='Ex', mode=1, file_name='mode_antisymmetric', file_type='png')
-em.close()
-
-rows = sorted(report['_default'].values(), key=lambda row: row[1], reverse=True)
-n_sym, n_anti = rows[0][1], rows[1][1]
-kappa1_per_nm = (np.pi / WAVELENGTH_NM) * (n_sym - n_anti)
-print(f"gap = {GAP_NM} nm: n_sym = {n_sym:.6f}, n_anti = {n_anti:.6f}, kappa1 = {kappa1_per_nm:.4e} 1/nm")
+# rows = sorted(report['_default'].values(), key=lambda row: row[1], reverse=True)
+# n_sym, n_anti = rows[0][1], rows[1][1]
+# kappa1_per_nm = (np.pi / WAVELENGTH_NM) * (n_sym - n_anti)
+# print(f"gap = {GAP_NM} nm: n_sym = {n_sym:.6f}, n_anti = {n_anti:.6f}, kappa1 = {kappa1_per_nm:.4e} 1/nm")
 
 # %% [markdown]
 # ## Task 1: Gap sweep and exponential fit
